e8a87d8052f832883d2f93c4d515ba87bc85cefc
abhishekvyas2399 <abhishekvyas2399@gmail.com>
2025-06-23 22:06:02 +0530

    video call feature with some bug

--- socket.js
+++ socket.js
@@ -24,5 +24,33 @@ module.exports=(io)=>{
         socket.on("disconnect",(socket)=>{
             io.emit("userStatus",{userId:socket.id,status:"offline"}); // status offline when disconnect
         });
+
+
+        // video call request/answer
+        socket.on("videoCall",(chatId)=>{
+            const room=io.sockets.adapter.rooms.get(chatId);
+            if(!room || room.size==1)    io.to(chatId).emit("videoCallAnswer",{call:false,offline:true});
+            else    socket.to(chatId).emit("videoCall",chatId);
+        });
+        socket.on("videoCallAnswer",({chatId,call})=>{
+            socket.to(chatId).emit("videoCallAnswer",{call,offline:false});
+        });
+        socket.on("endVideoCall",(chatId)=>{
+            socket.to(chatId).emit("endVideoCall",chatId);
+        });
+
+        // video call webRTC connection establishment socket event
+        socket.on("offer",({roomId,offer})=>{
+            socket.to(roomId).emit("offer",{roomId,offer});
+            console.log("get an offer");
+        });
+        socket.on("answer",({roomId,answer})=>{
+            socket.to(roomId).emit("answer",answer);
+            console.log("get answer");
+        });
+        socket.on("ice-candidate",({roomId,iceCandidate})=>{
+            socket.to(roomId).emit("ice-candidate",iceCandidate);
+            console.log("get ice candidates");
+        });
     });
 }
